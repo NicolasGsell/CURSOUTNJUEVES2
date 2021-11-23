38bd7c5fe5e6c148632edb124de413af4b9520b4
Nicolas Gsell <nicocbj96@gmail.com>
2021-11-23 20:11:56 -0300

    realice unidad 3

---
+++ M3/UNIDAD 3-ALIMENTOS QUEEN/routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+const res = require('express/lib/response');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post("/", async (req, res, next) =>{
+
+var nombre = req.body.nombre;
+var apellido = req.body.apellido;
+var email = req.body.email;
+var telefono = req.body.tel;
+var mensaje = req.body.comentarios;
+
+//console.log(req.body);
+
+var obj = {
+to: "nicocbj96@gmail.com",
+subject: "Contacto desde la web",
+html: nombre + " " + apellido + " se contacto a traves y quiere mas info a este correo: " + email + 
+".<br> Ademas, hizo el siguiente comentario: " + mensaje + ". <br> Su tel es " + telefono
+}
+
+var transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+})
+ 
+  var info = await transport.sendMail(obj);
+
+res.render("index", {
+    message: "Mensaje enviado correctamente",
+});
+
+});
+
+module.exports = router;
